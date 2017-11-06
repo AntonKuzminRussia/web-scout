17e0ec0daa2b51c53291bf868daed48ffa5bdf92
a.kuzmin <anton.kuzmin@marketgid.com>
2017-11-06 09:34:57 +0300

    It`s crutch time!

--- classes/kernel/WSCounter.py
+++ classes/kernel/WSCounter.py
@@ -30,30 +30,33 @@ class WSCounter(object):
 
     def up(self):
         """ Up counter, print result if need """
-        self.counter += 1
+        try:
+            self.counter += 1
 
-        if self.counter%self.point == 0:
-            sys.stdout.write('.')
+            if self.counter%self.point == 0:
+                sys.stdout.write('.')
 
-        if self.counter%self.new_str == 0:
-            if self.all == 0:
-                print "({0})".format(self.counter)
-            else:
-                percents = round(self.counter/(self.all/100)) if round(self.counter/(self.all/100)) > 0 else 1
-                # Костыль если 0 секунд прошло, а уже есть что выводить
-                time_now = int(time.time()) - self.start_time if int(time.time()) - self.start_time > 0 else 1
-                time_left = round((100-percents)*(time_now/percents))
+            if self.counter%self.new_str == 0:
+                if self.all == 0:
+                    print "({0})".format(self.counter)
+                else:
+                    percents = round(self.counter/(self.all/100)) if round(self.counter/(self.all/100)) > 0 else 1
+                    # Костыль если 0 секунд прошло, а уже есть что выводить
+                    time_now = int(time.time()) - self.start_time if int(time.time()) - self.start_time > 0 else 1
+                    time_left = round((100-percents)*(time_now/percents))
 
-                print "({0}/{1}/{2}%) | {3} | {4} | {5} o/s".format(
-                    nformat(self.counter),
-                    nformat(self.all),
-                    round(self.counter/(self.all/100), 2),
-                    secs_to_text(time_now),
-                    secs_to_text(time_left),
-                    round((self.counter - self.last_point_count) / (int(time.time()) - self.last_point_time), 2) #round(self.counter/time_now, 2)
-                )
-                self.last_point_count = self.counter
-                self.last_point_time = int(time.time())
+                    print "({0}/{1}/{2}%) | {3} | {4} | {5} o/s".format(
+                        nformat(self.counter),
+                        nformat(self.all),
+                        round(self.counter/(self.all/100), 2),
+                        secs_to_text(time_now),
+                        secs_to_text(time_left),
+                        round((self.counter - self.last_point_count) / (int(time.time()) - self.last_point_time), 2) #round(self.counter/time_now, 2)
+                    )
+                    self.last_point_count = self.counter
+                    self.last_point_time = int(time.time())
+        except ZeroDivisionError:
+            pass
 
         sys.stdout.flush()
 
