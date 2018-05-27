7f6815842a1ae9c3c2a136337907aee926f78411
anton <anton.kuzmin.russia@gmail.com>
2018-05-27 17:23:07 +0300

    XmlOutput

--- classes/XmlOutput.py
+++ classes/XmlOutput.py
@@ -11,11 +11,9 @@ Class of xml ouput functional
 
 import xml.etree.cElementTree as eTree
 
+
 class XmlOutput:
     report_name = None
-
-    progress_root = None
-    progress_doc = None
 
     errors_root = None
     errors_doc = None
@@ -26,9 +24,6 @@ class XmlOutput:
     def __init__(self, report_name):
         self.report_name = report_name
 
-        self.progress_root = eTree.Element("root")
-        self.progress_doc = eTree.SubElement(self.progress_root, "progress")
-
         self.errors_root = eTree.Element("root")
         self.errors_doc = eTree.SubElement(self.errors_root, "errors")
 
@@ -36,16 +31,21 @@ class XmlOutput:
         self.results_doc = eTree.SubElement(self.results_root, "results")
 
     def put_progress(self, count_now, full_count, percent_done, time_now, time_left, speed):
-        eTree.SubElement(self.progress_doc, "count_now").text = str(count_now)
-        eTree.SubElement(self.progress_doc, "full_count").text = str(full_count)
-        eTree.SubElement(self.progress_doc, "percent_done").text = str(percent_done)
-        eTree.SubElement(self.progress_doc, "time_now").text = str(time_now)
-        eTree.SubElement(self.progress_doc, "time_left").text = str(time_left)
-        eTree.SubElement(self.progress_doc, "speed").text = str(speed)
+        progress_root = eTree.Element("root")
+        progress_doc = eTree.SubElement(progress_root, "progress")
+        
+        eTree.SubElement(progress_doc, "count_now").text = str(count_now)
+        eTree.SubElement(progress_doc, "full_count").text = str(full_count)
+        eTree.SubElement(progress_doc, "percent_done").text = str(percent_done)
+        eTree.SubElement(progress_doc, "time_now").text = str(time_now)
+        eTree.SubElement(progress_doc, "time_left").text = str(time_left)
+        eTree.SubElement(progress_doc, "speed").text = str(speed)
 
-        eTree.ElementTree(self.progress_root).write(self.report_name + "-progress.xml")
+        eTree.ElementTree(progress_root).write(self.report_name + "-progress.xml")
 
     def put_result(self, data):
+        for field in data.keys():
+            data[field] = str(data[field])
         item = eTree.SubElement(self.results_doc, "item")
         for field in data.keys():
             eTree.SubElement(item, field).text = data[field]
