100db6dfbacba5f36dd446689e7b43d5eafd31ac
anton <anton.kuzmin.russia@gmail.com>
2019-03-29 08:45:54 +0300

    doc strings

--- classes/threads/HttpThread.py
+++ classes/threads/HttpThread.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+This is part of WebScout software
+Docs EN: http://hack4sec.pro/wiki/index.php/WebScout_en
+Docs RU: http://hack4sec.pro/wiki/index.php/WebScout
+License: MIT
+Copyright (c) Anton Kuzmin <http://anton-kuzmin.ru> (ru) <http://anton-kuzmin.pro> (en)
+
+Thread class for HostsBrute modules
+"""
+
 import copy
 
 from libs.common import is_binary_content_type
@@ -7,9 +18,21 @@ from libs.common import get_response_size
 
 
 class HttpThread(AbstractThread):
+    """ Common parent for all threads working with raw http (not by selenium) """
     http = None
 
+    def __init__(self):
+        AbstractThread.__init__(self)
+        self.http = copy.deepcopy(Registry().get('http'))
+
     def test_log(self, url, resp, positive_item):
+        """
+        Log data for test mode
+        :param url:
+        :param resp:
+        :param positive_item:
+        :return:
+        """
         if self.is_test():
             self.test_put(
                 url,
@@ -21,22 +44,33 @@ class HttpThread(AbstractThread):
                 }
             )
 
-    def __init__(self):
-        AbstractThread.__init__(self)
-        self.http = copy.deepcopy(Registry().get('http'))
-
     def is_response_content_binary(self, resp):
+        """
+
+        :param resp:
+        :return:
+        """
         return resp is not None \
             and 'content-type' in resp.headers \
             and is_binary_content_type(resp.headers['content-type'])
 
     def get_headers_text(self, resp):
+        """
+        Join all headers from Response object in one string (http represenation with \r\n)
+        :param resp:
+        :return:
+        """
         response_headers_text = ''
         for header in resp.headers:
             response_headers_text += '{0}: {1}\r\n'.format(header, resp.headers[header])
         return response_headers_text
 
     def is_response_right(self, resp):
+        """
+        Return true if response has not false or not-found respose attribute(s)
+        :param resp:
+        :return:
+        """
         if resp is None:
             return False
 
@@ -54,6 +88,13 @@ class HttpThread(AbstractThread):
         return True
 
     def log_item(self, item_str, resp, is_positive):
+        """
+        Logging checked item
+        :param item_str:
+        :param resp:
+        :param is_positive:
+        :return:
+        """
         if isinstance(resp, basestring):
             log_content = resp
         else:
@@ -67,10 +108,22 @@ class HttpThread(AbstractThread):
         )
 
     def check_positive_limit_stop(self, result, rate=1):
+        """
+        Does we have too many positive results now?
+        :param result:
+        :param rate:
+        :return:
+        """
         if len(result) >= (int(Registry().get('config')['main']['positive_limit_stop']) * rate):
             Registry().set('positive_limit_stop', True)
 
     def is_retest_need(self, word, resp):
+        """
+        Return true if response has attributes of retest need
+        :param word:
+        :param resp:
+        :return:
+        """
         try:
             if resp is not None:
                 if (len(self.retest_codes) and str(resp.status_code) in self.retest_codes) or \
