f3911c22817d6954e94fda2f147907467cbc5c7b
anton <anton.kuzmin.russia@gmail.com>
2019-04-05 09:14:29 +0300

    no long wait bad proxy

--- classes/Proxies.py
+++ classes/Proxies.py
@@ -50,9 +50,9 @@ class Proxies(object):
                 "https": "https://" + proxy,
             }
             if Registry().isset('url_for_proxy_check'):
-                requests.get(Registry().get('url_for_proxy_check'), timeout=10, allow_redirects=False, proxies=proxies)
+                requests.get(Registry().get('url_for_proxy_check'), timeout=5, allow_redirects=False, proxies=proxies)
             else:
-                requests.get('http://google.com', timeout=10, allow_redirects=False, proxies=proxies)
+                requests.get('http://google.com', timeout=5, allow_redirects=False, proxies=proxies)
             #print 'Proxy {0} is alive'.format(proxy)
             self.died_count = 0
             return True
