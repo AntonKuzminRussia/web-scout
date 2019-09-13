837936791755a1de5e562aa41434f94a25ed5d33
anton <anton.kuzmin.russia@gmail.com>
2019-09-13 17:42:40 +0300

    fix output

--- classes/modules/ParamsBruterModules.py
+++ classes/modules/ParamsBruterModules.py
@@ -79,5 +79,7 @@ class ParamsBruterModules(WSModule):
         """
         WSModule.output(self)
 
+        self.logger.log("\n")
+        self.logger.log("Params found:\n")
         for result_row in self.result:
             self.logger.log("\t" + result_row)
